384bb4f5755653caef6041a13f98d02e3614fafe
runonwalls <hmorgan471@gmail.com>
2015-03-09 20:29:18 -0700

    finished the next phase (form -> post request -> post route -> piglatin response), and some bugfixes.

--- app/script.js
+++ app/script.js
@@ -22,8 +22,34 @@ $(document).ready(function() {
 
   $("#cooljsonparty").on("click",function(){
       $.get("http://localhost:3000/hesitantthinking", function(response){
-        $("#ajax-json-text")
+        $("#ajax-json-text").text(response); //not working as intended right now :()
       });
     });
-  } // end wrapper function
-); // end .ready
+
+  //Our event handler, that fires when the form is submitted
+  $("#piglatin").on("submit", function(e) {
+    e.preventDefault(); //on submit, when you click on them, prevents their default action
+    //(the default action for jquery on submit is to refresh your entire page. We don't want that!
+    // so we add this line to keep jQuery from doing what it wants to do.)
+
+    //this selector grabs everything that's an input tag,
+    //and THEN grabs everything that has the specified attribute pair (name=firstname)
+    var firstName = $("input[name=firstname]").val(); //val grabs the input data from that field
+    var lastName = $("input[name=lastname]").val(); //(i.e. what the user typed in)
+
+    var name = { firstName: firstName, lastName: lastName };
+
+    //...and now, we use $.post to send the data to the server and
+    //update the <p> tag when the response comes back.
+
+    //("piglatin" is the route on our server. we don't app.get that route, but we DO app.post,
+      //meaning that when we post to it, the app.post("/piglatin") in server.js will run,
+      //getting the data we sent!)
+    $.post("piglatin", name, function(response) {
+      var pigLatinName = response.firstname + " " + response.lastname;
+      $("#pigLatinName").text(pigLatinName);
+    });
+  });
+
+  } // end ready function
+); // end $(document).ready
--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var app = express(); //app is now something we can use dot notation with, to use
 var port = process.env.PORT || 3000; //tells Express to listen for http requests on port 3000
                                      //(unless we've set a different port in an environmental variable)
 
+var bodyparser = require("body-parser"); //this node module will automatically parse .json for us
+app.use(bodyparser.json()); //These two lines tell the express app to use the bodyparser module...
+app.use(bodyparser.urlencoded({extended: true})); //...and give it some needed settings.
+  //(it needs this to actually be able to decode the data coming in on a request.)
+
+
 //The first parameter is the route, the second is the function. (Express, like jQuery, has language you'll need to learn.)
 // app.get('/', function(req, res){ //req, res means request and response. 
 //   res.send("hello, universe"); //Our response is gonna send this string.
@@ -42,6 +48,42 @@ app.get('/hesitantthinking', function(req, res){
   var randomIndex = Math.floor(Math.random() * thoughts.length);
   res.json(thoughts[randomIndex]);
 });
+
+//"hey app, make us a post route (expect incoming traffic on this route)""
+//post routes take two params: a route name, and a function, executed when someone hits that route
+//bodyparser gives us access to the req parameter. specifically, request.body, which is the name that we
+//sent from the $.post in our front end!
+app.post("/piglatin", function(req, res) {
+  var firstname = piglatinify(req.body.firstName);
+  var lastname = piglatinify(req.body.lastName);
+  var piglatined = {firstname: firstname, lastname:
+  lastname};
+  res.json(piglatined);
+});
+
+function piglatinify(word) {
+  var wordArray = word.split('');
+  var letters;
+  var changedWord; //the pig-latinified word we return
+
+  //Is the first letter a vowel?
+  var vowelHash = {a: 1, e: 1, i: 1, o: 1, u: 1,
+                   A: 1, E: 1, I: 1, O: 1, U: 1};
+  //hasOwnProperty is the way you look up a property on an object! It *WILL NOT* go up the prototype chain!
+  if (vowelHash.hasOwnProperty(wordArray[0])) {
+    return word + "-hay";
+  }
+  //This avoids a huge switch-statement or if-checks. Just one hash, and one hash lookup!
+
+  //otherwise, it starts with a consonant
+  letters = wordArray.shift();
+  changedWord = wordArray.join("") + "-" + letters + "ay";
+  return changedWord;
+
+
+  return "hello";
+}
+
 /////////////////////END OF COOL EXPERIMENT ZONE/////////////////////////////
 
 //This should always be the last thing in your server file. You want to have everything set up and all your elements built
